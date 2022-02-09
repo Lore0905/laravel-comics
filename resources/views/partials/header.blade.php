ad7f52965dd60200e9605a570c12658706801c61
Lore0905 <lore.castelli4@gmail.com>
2022-02-09 16:28:34 +0100

    first commit

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,46 @@
+
+
+<header class="container">
+
+    <div class="col col-sx">
+        <img id="logo" src="{{ asset('images/dc-logo.png')}}" alt="logo">
+    </div>
+    
+    <div class="col col-dx">
+        <ul>
+            <li >
+                <a href="#">CHARACTERS</a>
+            </li>
+            <li >
+                <a href="#">COMICS</a>
+            </li>
+            <li >
+                <a href="#">MOVIES</a>
+            </li>
+            <li >
+                <a href="#">TV</a>
+            </li>
+            <li >
+                <a href="#">GAMES</a>
+            </li>
+            <li >
+                <a href="#">COLLECTIBLES</a>
+            </li>
+            <li >
+                <a href="#">VIDEOS</a>
+            </li>
+            <li >
+                <a href="#">FANS</a>
+            </li>
+            <li >
+                <a href="#">NEWS</a>
+            </li>
+            <li >
+                <a href="#">SHOP</a>
+            </li>
+            
+        </ul>
+    </div>
+
+</header>
+    
